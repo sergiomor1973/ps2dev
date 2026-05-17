83f139704db853038844e7d62519e85cc445b596
sergiomor1973 <159595103+sergiomor1973@users.noreply.github.com>
2026-05-17 12:08:25 +0100

    Update ps2dev-config.sh

--- config/ps2dev-config.sh
+++ config/ps2dev-config.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-PS2TOOLCHAIN_REPO_URL="https://github.com/ps2dev/ps2toolchain.git"
+PS2TOOLCHAIN_REPO_URL="https://github.com/sergiomor1973/ps2toolchain.git"
 PS2TOOLCHAIN_DEFAULT_REPO_REF="master"
-PS2SDK_REPO_URL="https://github.com/ps2dev/ps2sdk.git"
+PS2SDK_REPO_URL="https://github.com/sergiomor1973/ps2sdk.git"
 PS2SDK_DEFAULT_REPO_REF="master"
-PS2SDK_PORTS_REPO_URL="https://github.com/ps2dev/ps2sdk-ports.git"
+PS2SDK_PORTS_REPO_URL="https://github.com/sergiomor1973/ps2sdk-ports.git"
 PS2SDK_PORTS_DEFAULT_REPO_REF="master"
-PS2_PACKER_REPO_URL="https://github.com/ps2dev/ps2-packer.git"
+PS2_PACKER_REPO_URL="https://github.com/sergiomor1973/ps2-packer.git"
 PS2_PACKER_DEFAULT_REPO_REF="master"
-PS2CLIENT_REPO_URL="https://github.com/ps2dev/ps2client.git"
+PS2CLIENT_REPO_URL="https://github.com/sergiomor1973/ps2client.git"
 PS2CLIENT_DEFAULT_REPO_REF="master"
 
 if test -f "$PS2DEV_CONFIG_OVERRIDE"; then
